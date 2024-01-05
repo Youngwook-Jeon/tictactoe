b094283868a856e46582361944a4d01fc2c5ca4b
Youngwook-Jeon <ela87@naver.com>
2024-01-05 22:53:41 +0900

    Add decoration for buttons

--- tictactoewidget.cpp
+++ tictactoewidget.cpp
@@ -3,21 +3,69 @@
 TicTacToeWidget::TicTacToeWidget(QWidget *parent)
     : QWidget(parent)
 {
+    player = Player::Player1;
     createBoard();
 }
 
 TicTacToeWidget::~TicTacToeWidget() {}
 
+void TicTacToeWidget::resetBoard()
+{
+    for (int i = 0; i < MetaData::ROWS * MetaData::COLUMNS; i++) {
+        board.at(i)->setText(MetaData::SPACE_CHAR);
+    }
+}
+
+void TicTacToeWidget::setCurrentPlayer(Player p)
+{
+    player = p;
+}
+
+Player TicTacToeWidget::getPlayer() const
+{
+    return player;
+}
+
+void TicTacToeWidget::handleClicksOnBoard(int buttonIndex)
+{
+    if (buttonIndex < 0 || buttonIndex >= board.size()) return;
+
+    QPushButton* button = board.at(buttonIndex);
+    button->setFont(QFont("Tahoma", 12, QFont::Bold));
+
+    if (button->text() != MetaData::SPACE_CHAR) return;
+
+    QString symbol;
+
+    if (player == Player::Player1) {
+        symbol = "X";
+        button->setText(symbol);
+        button->setStyleSheet("QPushButton{color: blue; background: lightyellow;}");
+        button->setDisabled(true);
+        setCurrentPlayer(Player::Player2);
+    } else if (player == Player::Player2) {
+        symbol = "O";
+        button->setText(symbol);
+        button->setStyleSheet("QPushButton{color: red; background: lightgreen;}");
+        button->setDisabled(true);
+        setCurrentPlayer(Player::Player1);
+    }
+}
+
 void TicTacToeWidget::createBoard()
 {
     QGridLayout* grid = new QGridLayout(this);
+    QSignalMapper* mapper = new QSignalMapper();
     for (int row = 0; row < MetaData::ROWS; row++) {
         for (int column = 0; column < MetaData::COLUMNS; column++) {
             QPushButton* button = new QPushButton(this);
-            button->setText(" ");
+            button->setText(MetaData::SPACE_CHAR);
             button->setMinimumHeight(50);
             button->setMinimumWidth(50);
             grid->addWidget(button, row, column);
+            mapper->setMapping(button, board.size());
+            connect(button, SIGNAL(clicked()), mapper, SLOT(map()));
+            connect(mapper, SIGNAL(mappedInt(int)), this, SLOT(handleClicksOnBoard(int)));
             board.append(button);
         }
     }
--- tictactoewidget.h
+++ tictactoewidget.h
@@ -5,10 +5,16 @@
 #include <QList>
 #include <QPushButton>
 #include <QGridLayout>
+#include <QSignalMapper>
 
 struct MetaData {
     static constexpr int ROWS = 3;
     static constexpr int COLUMNS = 3;
+    static constexpr const char* SPACE_CHAR = " ";
+};
+
+enum Player {
+    Player1, Player2
 };
 
 class TicTacToeWidget : public QWidget
@@ -18,11 +24,18 @@ class TicTacToeWidget : public QWidget
 public:
     TicTacToeWidget(QWidget *parent = nullptr);
     ~TicTacToeWidget();
+    void resetBoard();
+    void setCurrentPlayer(Player p);
+    Player getPlayer() const;
+
+private slots:
+    void handleClicksOnBoard(int);
 
 private:
     void createBoard();
 
 private:
     QList<QPushButton*> board;
+    Player player;
 };
 #endif // TICTACTOEWIDGET_H
